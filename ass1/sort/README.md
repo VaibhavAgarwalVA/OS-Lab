259d2ee98d168c753b2284ef60d2518efef1d3e7
VaibhavAgarwalVA <vaiagarwal96@gmail.com>
2017-01-11 19:02:47 +0530

    added sort. more managed

---
+++ ass1/sort/README.md
@@ -0,0 +1,3 @@
+Compile : gcc -o sort1 sort1.c    [ gcc -o *executable file name* *source file* ]
+Run : ./sort1 anurag.txt          [ *executable file name* *file name* ]
+
---
+++ ass1/sort/sort1.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+#define LENARR 1000
+ 
+void main(int argc, char *argv[])
+{
+	char filename[LENARR];
+	strcpy(filename, argv[1]);
+	
+	int arr[LENARR],n,i,j,temp;
+	n=0;
+	
+	FILE *fp;
+	fp = fopen(filename,"r");
+	while(fscanf(fp,"%d",&arr[i++])!=EOF)
+		n++;
+	fclose(fp);
+	
+
+	for(i=0;i<n-1;i++){
+		for(j=0;j<n-i-1;j++){
+			if(arr[j]>arr[j+1]){
+				temp=arr[j];
+				arr[j]=arr[i];
+				arr[i]=temp;
+			}
+		}
+	}
+
+	printf("Sorted list in ascending order: ");
+
+	for(i=0;i<n;i++)
+		printf("%d ",arr[i]);
+		
+	printf("\n");	
+}
