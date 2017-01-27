a476a75808bc177b21b27a96f6ae808ebb4addb0
VaibhavAgarwalVA <vaiagarwal96@gmail.com>
2017-01-27 18:50:10 +0530

    changed input format

--- ass2/Part2/shell.c
+++ ass2/Part2/shell.c
@@ -15,16 +15,45 @@
 int main()
 {
 	do{
-		char str[STRMAX], cwd[STRMAX];
-		int status, length; 
-		
+		char str[STRMAX], cwd[STRMAX], full[STRMAX];
+		int status, length, i, l, j; 
+
 		getcwd(cwd,100);
-		printf("%s > ",cwd);
-		scanf("%s",str);
+		printf(" %s > ",cwd);
+		scanf(" %[^\n]",full);
+
+		//printf("**%s**\t",full);
+
+		l = strlen(full);
+
+		i=0;
+		while(str[i])
+			str[i++]='\0';
+
+		j=0;
+		i=0;
+		while(full[i]==' ' && i<l) i++;	
+		for(;i<l;i++){
+			if(full[i]==' ')
+				break;
+			str[j++]=full[i];
+		}
+		while(full[i]==' ' && i<l) i++;
+
+		// printf("**%s**\n",str);
 
 		if(strcmp(str,"cd")==0){
 			char cd[STRMAX];
-			scanf("%s",cd);
+			j=0;
+			while(cd[j])
+				cd[j++]='\0';
+			
+			j=0;
+			while(full[i]==' ' && i<l) i++;
+			for(;i<l;i++)
+				cd[j++]=full[i];
+			while(full[i]==' ' && i<l) i++;
+
 			status = chdir(cd);
 			if(status == -1) 
 				perror("Error in changing directory!\n");
@@ -34,29 +63,67 @@ int main()
 		}
 		else if(strcmp(str,"mkdir")==0){
 			char name[STRMAX];
-			scanf("%s",name);
+			j=0;
+			while(name[j])
+				name[j++]='\0';
+
+			j=0;
+			while(full[i]==' ' && i<l) i++;
+			for(;i<l;i++)
+				name[j++]=full[i];
+			while(full[i]==' ' && i<l) i++;
+
 			status = mkdir(name, S_IRWXU | S_IRWXG | S_IRWXO);
 			if(status==-1)
 				perror("Error in creating new directory!\n");
 		}
 		else if(strcmp(str,"rmdir")==0){
 			char name[STRMAX];
-			scanf("%s",name);
+			j=0;
+			while(name[j])
+				name[j++]='\0';
+
+			j=0;
+			while(full[i]==' ' && i<l) i++;
+			for(;i<l;i++)
+				name[j++]=full[i];
+			while(full[i]==' ' && i<l) i++;
+			
 			status = rmdir(name);
 			if(status==-1)
 				perror("Error in removing directory!\n");
 		}
 		else if(strcmp(str,"ls")==0){
-			DIR *dp;
-			struct dirent *sd; 
-			dp=opendir(cwd);
-			while((sd=readdir(dp))!=NULL){
-				if(strcmp(sd->d_name,"..")==0 || strcmp(sd->d_name,".")==0)
-					continue;
-				printf("%s\n",sd->d_name);
+			char extra[STRMAX];
+			j=0;
+			while(extra[j])
+				extra[j++]='\0';
+
+			j=0;
+			while(full[i]==' ' && i<l) i++;
+			for(;i<l;i++)
+				extra[j++]=full[i];
+			while(full[i]==' ' && i<l) i++;
+
+			if(j==0){
+				DIR *dp;
+				struct dirent *sd; 
+				dp=opendir(cwd);
+				while((sd=readdir(dp))!=NULL){
+					if(strcmp(sd->d_name,"..")==0 || strcmp(sd->d_name,".")==0)
+						continue;
+					printf("%s\n",sd->d_name);
+				}
+				printf("========================\n");
+				closedir(dp);
 			}
-			printf("========================\n");
-			closedir(dp);
+			else{
+				if(strcmp(extra,"-l")==0){
+					printf("-l\n");
+				}
+				else
+					printf("Incorrect command format!\n");
+			}
 		}
 		else if(strcmp(str,"cp")==0){
 			printf("cp\n");
